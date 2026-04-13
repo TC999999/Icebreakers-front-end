23a5e2722a3438127c12e332296558122b1c652f
Tyler <insertcleverusernamehere999@gmail.com>
2026-04-13 11:42:09 -0400

    optimistic updates in direct request inbox on receiving socket signal from another user or when responding to a request

--- src/requests/hooks/useRequestListPageDirectRequest.tsx
+++ src/requests/hooks/useRequestListPageDirectRequest.tsx
@@ -1,23 +1,22 @@
-import { useCallback } from "react";
 import { useAppDispatch, useAppSelector } from "../../features/hooks";
 import { type AppDispatch } from "../../features/store";
 import { setUnansweredRequests } from "../../features/slices/auth";
 import directRequestsAPI from "../../apis/directRequestsAPI";
 import type {
-  sentRequestCard,
   directConversationResponse,
-  requestSocketHookProps,
   requestInfiniteQueryRes,
+  requestSocketHookProps,
 } from "../../types/requestTypes";
 import { shallowEqual } from "react-redux";
+import { useMutation } from "@tanstack/react-query";
 import queryClient from "../../helpers/queryClient";
 import type { InfiniteData } from "@tanstack/react-query";
 
 // custom hook for direct request inbox in request inbox page
 const useRequestListPageDirectRequests = ({
-  requests,
-  requestCount,
   setNewRequestCount,
+  refetchRequests,
+  requestParams,
 }: requestSocketHookProps) => {
   const dispatch: AppDispatch = useAppDispatch();
 
@@ -25,50 +24,37 @@ const useRequestListPageDirectRequests = ({
     return store.user.user?.username;
   }, shallowEqual);
 
-  // updates current request list
-  const refetchRequests = (id: string, type: string, accepted?: boolean) => {
-    console.log(id, accepted);
-    queryClient.setQueryData(
-      [
-        "requests",
-        {
-          directOrGroup: "direct",
-          requestOrInvitation: "requests",
-          type,
-        },
-      ],
-      (prev: InfiniteData<requestInfiniteQueryRes, unknown>) => {
-        console.log(prev.pages);
-        const newMap = prev.pages.map((r) => {
-          const newRequests = r.requestList.map((request) => {
-            if (request.id === id)
-              return { ...request, hasAccepted: accepted, hasResponded: true };
-            return request;
-          });
-
-          return { ...r, requestList: newRequests };
-        });
-
-        return { ...prev, pages: newMap };
-      },
-    );
-  };
-
   // RESPONSES (RECIPIENT'S SIDE ONLY)
   // if request was accepted, the recipient join the group and
   // removes request from both users' inboxes
-  const respondToDirectRequest = useCallback(
-    async (response: directConversationResponse) => {
-      await directRequestsAPI.respondToDirectConversationRequest(
-        username!,
-        response,
-      );
+  const { mutate: respondToDirectRequest } = useMutation({
+    mutationFn: (response: directConversationResponse) =>
+      directRequestsAPI.respondToDirectConversationRequest(username!, response),
+
+    onMutate: async (response: directConversationResponse) => {
+      await queryClient.cancelQueries({
+        queryKey: ["requests", requestParams],
+      });
+      const previousPages = queryClient.getQueryData<
+        InfiniteData<requestInfiniteQueryRes, number>
+      >(["requests", requestParams]);
+      refetchRequests(response.id);
+      return { previousPages };
+    },
+    onSuccess: () => {
       setNewRequestCount();
-      refetchRequests(response.id, "received", response.accepted);
       dispatch(setUnansweredRequests(-1));
     },
-    [requests, dispatch],
-  );
+    onError: (err: Error, response: directConversationResponse, context) => {
+      queryClient.setQueryData(
+        ["requests", requestParams],
+        context?.previousPages,
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["requests", requestParams] });
+    },
+  });
 
   return {
     respondToDirectRequest,
--- src/requests/hooks/useRequestListPageSockets.tsx
+++ src/requests/hooks/useRequestListPageSockets.tsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import type {
   requestType,
   requestCount,
-  requestList,
+  requestInfiniteQueryRes,
   requestParams,
 } from "../../types/requestTypes";
 import socket from "../../helpers/socket";
 import useRequestListPageDirectRequests from "./useRequestListPageDirectRequest";
 import useRequestListPageGroupRequests from "./useRequestListPageGroupRequests";
+import type { InfiniteData } from "@tanstack/react-query";
 import queryClient from "../../helpers/queryClient";
 
 type Props = {
-  requests: requestList;
   requestCount: requestCount;
   viewedRequests: requestType;
   requestParams: requestParams;
@@ -19,70 +19,115 @@ type Props = {
 
 // custom hook for websocket logic in request inbox page
 const useRequestListPageSockets = ({
-  requests,
   viewedRequests,
   requestCount,
   requestParams,
 }: Props) => {
-  //callback function to set new request count: to only be used in other functions
-  const setNewRequestCount = () => {
-    queryClient.invalidateQueries({ queryKey: ["requestCount"] });
-  };
-
   // sets up socket listeners on initial render
   useEffect(() => {
-    socket.on("addRequest", () => {
+    socket.on("addRequest", async ({ request, requestType }) => {
+      if (requestType === viewedRequests) {
+        await queryClient.cancelQueries({
+          queryKey: ["requests", requestParams],
+        });
+
+        const { content, createdAt, from, id } = request;
+
+        queryClient.setQueryData(
+          ["requests", requestParams],
+          (prevData: InfiniteData<requestInfiniteQueryRes, number>) => {
+            const newRequest = {
+              content,
+              createdAt,
+              from,
+              id,
+              hasAccepted: false,
+              hasResponded: false,
+            };
+
+            return {
+              ...prevData,
+              pages: prevData.pages.map((page, i) => {
+                return i === 0
+                  ? { ...page, requestList: [newRequest, ...page.requestList] }
+                  : page;
+              }),
+            };
+          },
+        );
+
+        queryClient.invalidateQueries({
+          queryKey: ["requests", requestParams],
+        });
+      }
       setNewRequestCount();
     });
 
-    socket.on("removeRequest", ({ request, requestType }) => {
-      setNewRequestCount();
+    socket.on("removeRequest", async ({ response, requestType }) => {
       if (viewedRequests === requestType) {
-        queryClient.setQueryData(
-          ["requests", requestParams],
-          (prev: requestList[]) => {
-            return prev.filter((r) => {
-              return r[0].id !== request.id;
-            });
-          },
-        );
+        await queryClient.cancelQueries({
+          queryKey: ["requests", requestParams],
+        });
+
+        const { id } = response;
+        refetchRequests(id);
+        queryClient.invalidateQueries({
+          queryKey: ["requests", requestParams],
+        });
       }
+      setNewRequestCount();
     });
 
     return () => {
       socket.off("addRequest");
       socket.off("removeRequest");
     };
-  }, [viewedRequests, requestCount]);
+  }, [viewedRequests, requestCount, requestParams, queryClient]);
+
+  //callback function to set new request count: to only be used in other functions
+  const setNewRequestCount = () => {
+    queryClient.invalidateQueries({ queryKey: ["requestCount"] });
+  };
+
+  // updates current request list
+  const refetchRequests = useCallback(
+    (id: string) => {
+      queryClient.setQueryData(
+        ["requests", requestParams],
+        (prev: InfiniteData<requestInfiniteQueryRes, number>) => {
+          const newMap = prev.pages.map((r) => {
+            const newRequests = r.requestList.filter((request) => {
+              return request.id !== id;
+            });
+
+            return { ...r, requestList: newRequests };
+          });
+
+          return { ...prev, pages: newMap };
+        },
+      );
+    },
+    [requestParams, queryClient],
+  );
 
   // custom hook for updating direct request list (after responding to a request or deleting a request)
   const { respondToDirectRequest } = useRequestListPageDirectRequests({
-    requests,
-    requestCount,
     setNewRequestCount,
+    refetchRequests,
+    requestParams,
   });
 
-  const {
-    respondToGroupRequest,
-    removeGroupRequest,
-    deleteGroupRequest,
-    respondToGroupInvitation,
-    removeGroupInvitation,
-    deleteGroupInvitation,
-  } = useRequestListPageGroupRequests({
-    requests,
-    requestCount,
-    setNewRequestCount,
-  });
+  const { respondToGroupRequest, respondToGroupInvitation } =
+    useRequestListPageGroupRequests({
+      setNewRequestCount,
+      refetchRequests,
+      requestParams,
+    });
 
   return {
     respondToDirectRequest,
     respondToGroupRequest,
-    removeGroupRequest,
-    deleteGroupRequest,
     respondToGroupInvitation,
-    removeGroupInvitation,
-    deleteGroupInvitation,
   };
 };
 
